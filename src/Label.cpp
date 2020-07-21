0cadc26622bcb471da2a2f64359716886d277290
nicolpa <nicolas.mege@protonmail.com>
2020-07-21 19:38:57 +0200

    Fix label font not being set with the constructor

--- src/Label.cpp
+++ src/Label.cpp
@@ -4,6 +4,7 @@
 Label::Label(lcd::UTFT* LCD, uint16_t x, uint16_t y, String text, uint8_t* font, word color, uint32_t backcolor)
     : Component(LCD, x, y, 0, 0, color, backcolor), text(text)
 {
+    this->font = font;
     LCD->setFont(font);
     this->width = LCD->getFontXsize() * String(text).length();
 	this->height = LCD->getFontYsize();
@@ -13,6 +14,7 @@ Label::~Label() {}
 
 void Label::draw()
 {
+    Serial.println((font == nullptr) ? "euhm..." : "Okay");
     LCD->setFont(font);
     LCD->setColor(foreground);
     LCD->setBackColor(background);
